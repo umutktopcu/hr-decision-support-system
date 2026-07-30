17a2940f3f20440095f072e11021f8cb7256efe2
Umut Kaan Topcu <umutk.topcu@gmail.com>
2026-07-30 15:18:40 +0300

    chore: initialize layered .NET solution

---
+++ src/HrDecisionSupport.Infrastructure/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace HrDecisionSupport.Infrastructure;
+
+public class Class1
+{
+
+}
---
+++ src/HrDecisionSupport.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HrDecisionSupport.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
