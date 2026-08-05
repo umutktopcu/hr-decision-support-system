94b3355c49c916ac608adf3aa3a4665449fe92e1
Ege Oymak <egeoymak@outlook.com>
2026-08-05 14:53:39 +0300

    feat: add employee and candidate frontend

---
+++ src/HrDecisionSupport.Web/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HrDecisionSupport.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Dashboard";
+            return View();
+        }
+    }
+}
--- src/HrDecisionSupport.Web/Program.cs
+++ src/HrDecisionSupport.Web/Program.cs
@@ -27,8 +27,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 
 app.Run();
